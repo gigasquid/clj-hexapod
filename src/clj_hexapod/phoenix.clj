1ab1ba2b82617c22ccd741daa885fdf205bb1f75
gigasquid <cmeier@gigasquidsoftware.com>
2014-08-02 22:39:19 -0400

    basic working with phoneix code

--- src/clj_hexapod/phoenix.clj
+++ src/clj_hexapod/phoenix.clj
@@ -2,14 +2,14 @@
   (require [serial-port :as serial]))
 
 (def modes [:walk-mode :translate-mode :rotate-mode :single-leg-mode])
-(def LT-button 64)
-(def RT-button 128)
-(def L6-button 1)
-(def L5-button 2)
-(def L4-button 4)
-(def R3-button 8)
-(def R2-button 16)
-(def R1-button 32)
+(def LT-button 128)
+(def RT-button 64)
+(def L6-button 32)
+(def L5-button 16)
+(def L4-button8)
+(def R3-button 4)
+(def R2-button 2)
+(def R1-button 1)
 (def CENTER 128)
 (declare port)
 
@@ -30,7 +30,7 @@
    0
    (checksum [r-vert r-horz l-vert l-horz buttons])])
 
-(defn send [packet]
+(defn send-robot [packet]
   (serial/write port (vec->bytes packet)))
 
 (defn good-range? [speed]
@@ -38,10 +38,6 @@
 
 (def command-queue (atom []))
 (def default-command (build-packet CENTER CENTER CENTER CENTER 0))
-
-(defn send-commands [packet] (swap! command-queue (repeat 5 packet)))
-(defn send-button [packet] (swap! command-queue [packet]))
-
 
 
 ;;values between 129-254
@@ -70,43 +66,66 @@
 ;;Right Vertical always 128
 ;;middle at 128
 
+
+(defn send-command-from-queue []
+  (do (println "command queue is " @command-queue)
+      (let [command (or (first @command-queue) default-command)]
+        (println "Sending ..." command)
+        (reset! command-queue (rest @command-queue))
+        (send-robot command)
+        (Thread/sleep 33))))
+
+(defn add-command [command]
+  (swap! command-queue conj command))
+
+
 (defn sit-up []
   "raise up to a walking height"
-  (send-button (build-packet CENTER CENTER CENTER L5-button 0)))
+  (add-command (build-packet CENTER CENTER CENTER CENTER L5-button)))
 
 (defn walk-forward [speed]
   "walk forward speed between 1-100"
-  (send-command (build-packet CENTER CENTER (up speed) CENTER 0)))
+  (add-command (build-packet CENTER CENTER (up speed) CENTER 0)))
 
 (defn walk-backwards [speed]
   "walk backwards speed between 1-100"
-  (send-command (build-packet CENTER CENTER (down speed) CENTER 0)))
+  (add-command (build-packet CENTER CENTER (down speed) CENTER 0)))
 
 (defn walk-right [speed]
   "walk right speed between 1-100"
-  (send-command (build-packet CENTER CENTER CENTER (up speed) 0)))
+  (add-command (build-packet CENTER CENTER CENTER (up speed) 0)))
 
 (defn walk-left [speed]
   "walk right speed between 1-100"
-  (send-command (build-packet CENTER CENTER CENTER (down speed) 0)))
+  (add-command (build-packet CENTER CENTER CENTER (down speed) 0)))
 
 (defn turn-right [speed]
   "turn right speed between 1-100"
-  (send-command (build-packet CENTER (up speed) CENTER CENTER 0)))
+  (add-command (build-packet CENTER (up speed) CENTER CENTER 0)))
 
 (defn turn-left [speed]
   "turn left speed between 1-100"
-  (send-command (build-packet CENTER (down speed) CENTER CENTER 0)))
+  (add-command (build-packet CENTER (down speed) CENTER CENTER 0)))
 
 
 (defn stop []
   "stop hexapod"
-  (send-command (build-packet CENTER CENTER CENTER CENTER 0)))
+  (add-command (build-packet CENTER CENTER CENTER CENTER 0)))
 
 (defn good-bye []
   (serial/close port))
 
+(def talk-on (atom false))
+(def robot-agent (agent []))
 
+(defn start-communicator []
+  (send robot-agent (fn [_]
+                      (while @talk-on
+                        (send-command-from-queue)
+                        (Thread/sleep 300)))))
+
+
+(agent-errors robot-agent)
 (comment
 
   ;; Use this command to see what port your serial port
@@ -114,8 +133,25 @@
   (serial/list-ports)
 
   ;; replace the USB0 with whater it shows
-  (def port (serial/open "/dev/ttyUSB0" 38400))
+  (def port (serial/open "/dev/tty.usbserial-A60205ME" 38400))
 
+
+  (reset! talk-on true)
+  (start-communicator)
+  (reset! talk-on false)
+  
+
+
+  (sit-up)
+  (walk-forward 20)
+  @talk-on
+  @command-queue
+  (reset! talk-on false)
+
+  (reset! talk-on false)
+
+
+  
   (sit-up)
   (walk-forward 20)
   (walk-backwards 10)
@@ -123,7 +159,7 @@
   (walk-left 10)
   (turn-right 10)
   (turn-left 10)
-  
+
   (stop)
 
   (good-bye)
